febb53a1d34d89ee89cdc3cb3dce6674ca01a3e8
Julia Frederico <magalhaesjulia@id.uff.br>
2022-07-25 15:53:09 -0300

    adds server file structure serving public files

---
+++ server/server.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const { seed } = require('./seed.js');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static(path.join(__dirname, '../public')));
+app.use('/assets', express.static(path.join(__dirname, '../public/assets')));
+
+// DEV - SEED
+app.post('/seed', seed);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`up on ${port}`));
