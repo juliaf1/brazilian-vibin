e972be4a2659924394a04195f02e95c74bd15bbe
Julia Frederico <magalhaesjulia@id.uff.br>
2022-07-26 10:23:33 -0300

    adds spotify authorization code reference

--- server/services/spotify.js
+++ server/services/spotify.js
@@ -7,6 +7,7 @@ const client_secret = secrets.SPOTIFY_CLIENT_SECRET;
 const auth_token = Buffer.from(`${client_id}:${client_secret}`, 'utf-8').toString('base64');
 
 const getAuth = async () => {
+  // Reference: https://ritvikbiswas.medium.com/connecting-to-the-spotify-api-using-node-js-and-axios-client-credentials-flow-c769e2bee818
   try {
     const token_url = 'https://accounts.spotify.com/api/token';
     const data = qs.stringify({'grant_type':'client_credentials'});
