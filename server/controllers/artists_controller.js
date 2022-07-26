fdea779b314de3c8ebeaa0daa2c45e5af65a55df
Julia Frederico <magalhaesjulia@id.uff.br>
2022-07-26 10:20:57 -0300

    updates artists controller to use spotify custom service for authorization flow

--- server/controllers/artists_controller.js
+++ server/controllers/artists_controller.js
@@ -1,13 +1,8 @@
-const { sequelize, secrets } = require('../config.js');
 const axios = require('axios');
+const { sequelize } = require('../config.js');
+const { authorizationHeaders } = require('../services/spotify.js');
 
 const spotifyUrl = 'https://api.spotify.com/v1';
-
-tokenHeaders = {
-    headers: {
-        'Authorization': `Bearer ${secrets.SPOTIFY_TOKEN}`,
-    },
-};
 
 const random = async (req, res) => {
     const { genres } = req.query;
@@ -17,6 +12,8 @@ const random = async (req, res) => {
     
     let artists = [];
     let randomIds = [];
+
+    const headers = await authorizationHeaders();
 
     while (randomIds.length < maxSize) {
         let id = ids[0].sample().spotify_id;
@@ -29,8 +26,8 @@ const random = async (req, res) => {
         let artist = {};
 
         try {
-            const tracksRes = await axios.get(`${spotifyUrl}/artists/${id}/top-tracks?market=BR`, tokenHeaders);
-            const artistRes = await axios.get(`${spotifyUrl}/artists/${id}`, tokenHeaders)
+            const tracksRes = await axios.get(`${spotifyUrl}/artists/${id}/top-tracks?market=BR`, headers);
+            const artistRes = await axios.get(`${spotifyUrl}/artists/${id}`, headers)
             artist.track = tracksRes.data.tracks.sample();
             artist.info = artistRes.data;
         } catch {
